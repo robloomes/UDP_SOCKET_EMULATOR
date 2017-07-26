cf774a88580727b3300d5dd5825f780f0107e084
Jake-simpson13 <30329413+Jake-simpson13@users.noreply.github.com>
2017-07-26 13:52:48 +1200

    Update channel.py

--- channel.py
+++ channel.py
@@ -23,11 +23,37 @@ def channel(chan_send_in_port, chan_send_out_port, chan_receive_in_port,
     #connects the channel outoging sockets to their default recievers
     chan_send_out.connect(('localhost', sender_in_port))
     chan_receive_out.connect(('localhost', receiver_in_port))
+    #Where the loop function should go
+    loop(chan_send_in, chan_send_out, chan_recieve_in, 
+         chan_recieve_out, packet_loss_port)
     print("helpme")
     
     #TODO select() cs,in  cr,in
     #create loop which runs indefinitely
     #pseudo_random number based on P
-    
-    
-    
+        
+def loop(chan_send_in, chan_send_out, chan_receive_in, 
+         chan_receive_out, packet_loss_port):
+    """infinite loop function"""
+    while True:
+        expecting = select.select([chan_send_in, chan_receive_in])
+        for socket in expecting:
+            raw_packet = #something rather
+            
+        if random.random() < p_rate:
+            continue
+        elif socket == chan_send_in:
+            try:
+                chan_receive_out.#send the packet
+                
+            except ConnectionRefusedError:
+                #etc
+            else:
+                #its worked
+        elif socket == chan_receive_in:
+            try:
+                chan_send_out.#send the packet
+            except ConnectionRefusedError:
+                etc
+            else:
+                #its worked    
