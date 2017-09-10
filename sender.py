7f28a3e34f2645e2ed5cef4d806c3c5acea4733a
Jake-simpson13 <30329413+Jake-simpson13@users.noreply.github.com>
2017-09-11 11:37:50 +1200

    Update sender.py

--- sender.py
+++ sender.py
@@ -15,7 +15,58 @@ from contextlib import closing
 1 port number Cs,in
 file name
 """
+BLOCK_SIZE = int(os.environ.get('SENDER_BLOCK_SIZE', '512'))
+TIMEOUT = float(os.environ.get('SENDER_TIMEOUT', '1'))
 
+def loop(file_in, sock_in, sock_out):
+    v_next = 0
+    exit_flag = False
+    
+    while not exit_flag:
+        data = file_in.read(BLOCK_SIZE)
+        dataLen = len(data)
+        
+        if dataLen > 0:
+            packetBuffer = Packet(PacketType.data, v_next, data_len, data)
+        else:
+            if dataLen == 0:
+                packetBuffer = Packet(PacketType.data, v_next, data)
+                exitFlag = True
+        
+        exit_flag = not data
+        processing = True
+        while processing:
+            try:
+                sock_out.send(packetBuffer.to_bytes())
+            except ConnectionRefusedError:
+                print('Connection lost with sender')
+                return
+            ready, _, _ = select.select([sock_in], [], [], TIMEOUT)
+            
+            if not ready:
+                continue
+            
+            rcvd = sock_in.recv(2**16) #may change due to different number needed
+            
+            try:
+                exploded = Packet.from_bytes(rcvd)
+
+            except ValueError:
+                continue
+            if exploded.maginco != 0x497E:
+                continue
+            if exploded.type_ != PacketType.ack:
+                continue
+            if exploded.dataLen != 0:
+                continue
+            if exploded.seqno != v_next:
+                continue
+                
+            v_next = 1 - v_next
+            processing = False
+                                        
+                        
+                        
 def main(argv):
     try:
         s_in = parse_port(argv[1])
